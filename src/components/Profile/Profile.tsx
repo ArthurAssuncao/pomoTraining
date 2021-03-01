9c20617e3cf926492d7899e2800c06eb1483d4ec
Arthur Assuncao <arthur.xx92@gmail.com>
2021-03-01 09:47:59 -0300

    feat: add profile photo and name from GithubApi

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,19 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import levelIcon from "../../assets/img/icons/level.svg";
 import { ChallengesContext } from "../../contexts";
+import { GithubApi, GithubUserData } from "../../services/GithubApi";
 import styles from "./Profile.module.scss";
 
 const Profile = () => {
-  const { level, githubUsername } = useContext(ChallengesContext);
+  const { level, githubUsername, user, setUser } = useContext(
+    ChallengesContext
+  );
+
+  useEffect(() => {
+    if (!user) {
+      fetchGithubUserData();
+    }
+  }, []);
+
+  const fetchGithubUserData = async () => {
+    const githubApi = GithubApi;
+    const user: Promise<GithubUserData> = githubApi.user(githubUsername);
+    setUser(await user);
+  };
+
   return (
     <div className={styles.container}>
       <img
         className={styles.photo}
-        src={`https://github.com/${githubUsername}.png`}
+        src={user?.profilePhoto ?? `https://github.com/${githubUsername}.png`}
         alt="Profile"
       />
       <div className={styles.nameContainer}>
-        <strong className={styles.name}>{githubUsername}</strong>
+        <strong className={styles.name}>{user?.name ?? githubUsername}</strong>
         <p className={styles.nameIconWrapper}>
           <img className={styles.nameIcon} src={levelIcon} alt="Profile icon" />
           Level {level}
